f6a6c8e50b90074d629cd24471b333389f199f08
aceze <141794572+acezegith@users.noreply.github.com>
2024-03-28 17:52:04 +0300

    Update mileroPackage.lua v0.7

--- mileroPackage.lua
+++ mileroPackage.lua
@@ -22,40 +22,48 @@ local module = {}
 
 -- FETCHING MILERO API TO RETRIEVE USER DATA
 module.getUser = function(mainUserId)
-	local res = HttpService:GetAsync(API_URL .. "/api/hubs/" .. hubId .. "/accounts/" .. mainUserId .. "?apiCode=" .. accessCode .. "&userType=roblox")
+	local scucces, res = pcall(function()
+		return HttpService:GetAsync(API_URL .. "/api/hubs/" .. hubId .. "/accounts/" .. mainUserId .. "?apiCode=" .. accessCode .. "&userType=roblox")
+	end)
 	local data = HttpService:JSONDecode(res)
-	
+
 	return data.info
 end
 
 -- FETCHING MILERO API TO RETRIEVE HUB DATA
 module.getHub = function()
-	local res = HttpService:GetAsync(API_URL .. "/api/hubs/" .. hubId .. "?apiCode=" .. accessCode .. "&userType=roblox")
+	local scucces, res = pcall(function()
+		return HttpService:GetAsync(API_URL .. "/api/hubs/" .. hubId .. "?apiCode=" .. accessCode .. "&userType=roblox")
+	end)
 	local data = HttpService:JSONDecode(res)
-	
-	
+
 	return data.info
 end
 
 -- PUT REQUEST TO UPDATE USER IN HUB
 module.updateUser = function(userId, userData)
-	local res = HttpService:RequestAsync({
-		Url = (API_URL .. "/api/hubs/" .. hubId .. "/accounts/" .. userId .. "?apiCode=" .. accessCode .. "&userType=roblox"), -- This website helps debug HTTP requests
-		Method = "PUT",
-		Headers = {
-			["Content-Type"] = "application/json", -- When sending JSON, set this!
-		},
-		Body = HttpService:JSONEncode(userData),
-	})
+	local scucces, res = pcall(function()
+		return HttpService:RequestAsync({
+			Url = (API_URL .. "/api/hubs/" .. hubId .. "/accounts/" .. userId .. "?apiCode=" .. accessCode .. "&userType=roblox"), -- This website helps debug HTTP requests
+			Method = "PUT",
+			Headers = {
+				["Content-Type"] = "application/json", -- When sending JSON, set this!
+			},
+			Body = HttpService:JSONEncode(userData),
+		})
+	end)
+	
 	local data = HttpService:JSONDecode(res)
 
 	return data
-	
+
 end
 
 -- GET REQUEST TO OBTAIN USER'S GLOBAL ACCOUNT PUBLIC INFORMATION (USED MAINLY TO OBTAIN DATA LINKED TO DISCORD)
 module.getUserAccount = function(userId)
-	local res = HttpService:GetAsync(API_URL .. "/api/users/" .. userId .. "?apiCode=" .. accessCode .. "&reqType=roblox")
+	local scucces, res = pcall(function()
+		return HttpService:GetAsync(API_URL .. "/api/users/" .. userId .. "?apiCode=" .. accessCode .. "&reqType=roblox")
+	end)
 	local data = HttpService:JSONDecode(res)
 
 	return data.info
@@ -92,30 +100,5 @@ end
 
 -- MEDIUM LEVEL FUNCTIONS
 
-module.incrementMiles = function(userid, amount)
-	local success, user = pcall(function()
-		return module.getUser(userid)
-	end)
-
-	if(not success) then
-		return module.updateUser(userid, {miles = tostring(amount), flights = 0})
-	end
-
-	user.miles = user.miles + amount
-	module.updateUser(userid, user)
-end
-
-module.incrementFlights = function(userid, amount)
-	local success, user = pcall(function()
-		return module.getUser(userid)
-	end)
-
-	if(not success) then
-		return module.updateUser(userid, {flights = tostring(amount), miles = 0})
-	end
-
-	user.flights = user.flights + amount
-	module.updateUser(userid, user)
-end
-
+-- AWAITING V0.9 UPDATE
 return module
